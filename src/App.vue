f62db2ec2df3c176d8cf293145172bc53dcf0f94
Mattia311 <mattia.space98@gmail.com>
2021-11-15 11:54:20 +0100

    Installed bootstrap e axios

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,27 @@
 </template>
 
 <script>
+import axios from 'axios';
 
 
 export default {
   name: 'App',
   components: {
    
+  },
+  mounted () {
+    axios
+    .get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(r =>{
+      console.log(r);
+    }).catch(e=>{
+      console.log(e, 'ERROR');
+    })
   }
 }
 </script>
 
 <style lang="scss">
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
 
 </style>
