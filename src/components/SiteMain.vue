d6e51006420ea9b1c1a43c54532fd4a4d9882939
Mattia311 <mattia.space98@gmail.com>
2021-11-15 16:12:49 +0100

    ESERCIZIO COMPLETO

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="SiteMain">
-      <div class="row">
-          <div class="text-center" v-for="respons in response" :key="respons.poster">
-              <div class="respon">
-                  <img :src="respons" alt="">
+      <div class="d-flex flex-wrap justify-content-center">
+          <div class="" v-for="(song,index) in response" :key="index">
+              <div class="respon text-center ">
+                  <img  :src="song.poster" alt="">
+                  <h3>{{song.title}}</h3>
+                  <p>{{song.author}}</p>
+                  <p>{{song.year}}</p>
               </div>
           </div>
       </div>
@@ -23,8 +26,8 @@ export default {
     axios
     .get('https://flynn.boolean.careers/exercises/api/array/music')
     .then(r =>{
-      console.log(r.data);
-      this.response = r.data
+      console.log(r.data.response);
+      this.response = r.data.response
     }).catch(e=>{
       console.log(e, 'ERROR');
     })
@@ -37,7 +40,36 @@ export default {
 .SiteMain {
     background: #1E2D3B;
     width: 100%;
-    height: calc(100vh - 70px);
+    height: 1200px;
+    .d-flex {
+        padding-top: 70px;
+    }
+    .text-center {
+        width: 200px;
+        background: #2E3A46;
+        margin-left:20px;
+        margin-right: 20px;
+        margin-top: 20px;
+        padding-top: 10px;
+        height: 370px;
+        h3 {
+            color: white;
+            font-size: 28px;
+            margin-bottom: 20px;
+            margin-top: 20px;
+        }
+        p {
+            color: #807F7D;
+            margin-bottom: 0px;
+            
+        }
+       
+    }
+    
+    img {
+        width: 170px;
+         
+    }
 }
 
 </style>
