a6fd3dcec77c5a6d7bbf3ae3082c6bac9a968c74
Mattia311 <mattia.space98@gmail.com>
2021-11-16 12:46:08 +0100

    Inserito select

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -3,6 +3,15 @@
   <div class="row">
       <img src="../assets/Spotify_logo_without_text.svg" alt=""> 
   </div>
+  <div>
+    <select class="form-select" aria-label="Default select example">
+      <option selected>Open this select menu to view discs by your favorite genre</option>
+      <option value="1">Rock</option>
+      <option value="2">Pop</option>
+      <option value="3">Jazz</option>
+      <option value="4">Metal</option>
+    </select> 
+  </div>
 </div>
 </template>
 
@@ -17,10 +26,17 @@ export default {
     background: #2E3A46;
     width: 100%;
     height: 70px;
+    display: flex;
     img {
         width: 70px;
         margin-left: 20px;
-        margin-top: 12px;
+        margin-top: 2px;
+        
+    }
+    select {
+        width: 600px;
+        margin-top: 15px;
+        margin-left: 400px;
     }
 
    
